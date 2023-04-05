090053a52355dafda325b4bdead31773673f449d
sevdog <setti.davide89@gmail.com>
2023-04-05 16:57:49 +0200

    Small code performance and quality improvements

--- django_reverse_js/core.py
+++ django_reverse_js/core.py
@@ -2,7 +2,6 @@ import json
 from django.core.exceptions import ImproperlyConfigured
 from django.template import loader
 from django.utils.safestring import mark_safe
-from django.utils.encoding import force_str
 from django.urls import get_script_prefix, get_ns_resolver
 from .conf import settings
 
@@ -28,20 +27,18 @@ def prepare_url_list(urlresolver, namespace_path='', namespace=''):
         return
 
     if include_only_ns:
-        # Test urls without namespace
-        in_on_empty_ns = namespace == '' and '' in include_only_ns
-
-        # check if nestead ns isn't subns of include_only ns
-        # e.g. ns = "foo:bar" include_only = ["foo"] -> this ns will be used
-        # works for ns = "lorem:ipsum:dolor" include_only = ["lorem:ipsum"]
-        # ns "lorem" will be ignored but "lorem:ipsum" & "lorem:ipsum:.." won't
-        in_on_is_in_list = any(
-            ns != '' and simple_namespace.startswith(ns) for ns in include_only_ns
+        include_namespace = (
+            # urls without namespace
+            (not namespace and '' in include_only_ns)
+            # check that "\0" flag isn't used
+            # use "foo\0" to add urls just from "foo" not from subns "foo:bar"
+            or f'{simple_namespace}\0' in include_only_ns
+            # check if nestead ns isn't subns of include_only ns
+            # e.g. ns = "foo:bar" include_only = ["foo"] -> this ns will be used
+            # works for ns = "lorem:ipsum:dolor" include_only = ["lorem:ipsum"]
+            # ns "lorem" will be ignored but "lorem:ipsum" & "lorem:ipsum:.." won't
+            or any(ns and simple_namespace.startswith(ns) for ns in include_only_ns)
         )
-        # Test if isn't used "\0" flag
-        # use "foo\0" to add urls just from "foo" not from subns "foo:bar"
-        in_on_null = simple_namespace + '\0' in include_only_ns
-        include_namespace = in_on_empty_ns or in_on_is_in_list or in_on_null
     else:
         # by default include this namespace
         include_namespace = True
@@ -54,7 +51,7 @@ def prepare_url_list(urlresolver, namespace_path='', namespace=''):
                     for pattern in urlresolver.reverse_dict.getlist(url_name)
                     for pat in pattern[0]
                 ]
-                yield [namespace + url_name, url_patterns]
+                yield namespace + url_name, url_patterns
 
     # check for inner namespaces
     for inner_ns, (
@@ -77,57 +74,49 @@ def prepare_url_list(urlresolver, namespace_path='', namespace=''):
             inner_ns_path = ''
 
         # yield values from inner namespaces if any
-        for x in prepare_url_list(inner_urlresolver, inner_ns_path, inner_ns):
-            yield x
+        yield from prepare_url_list(inner_urlresolver, inner_ns_path, inner_ns)
 
 
 def generate_json(default_urlresolver, script_prefix=None):
     if script_prefix is None:
         script_prefix = get_script_prefix()
 
-    # Ensure consistent ouptut ordering
-    urls = sorted(prepare_url_list(default_urlresolver))
+    # re-arrange structure for JSON
+    # use a generator to reduce number of iterations
+    urls = (
+        [name, [[path, [arg for arg in args]] for path, args in patterns]]
+        for name, patterns in prepare_url_list(default_urlresolver)
+    )
     return {
-        'urls': [
-            [
-                force_str(name),
-                [
-                    [force_str(path), [force_str(arg) for arg in args]]
-                    for path, args in patterns
-                ],
-            ]
-            for name, patterns in urls
-        ],
+        # ensure consistent ouptut ordering
+        'urls': sorted(urls),
         'prefix': script_prefix,
     }
 
 
+_json_script_escapes = {
+    ord(">"): "\\u003E",
+    ord("<"): "\\u003C",
+    ord("&"): "\\u0026",
+}
+
+
 def _safe_json(obj):
-    return mark_safe(
-        json.dumps(obj)
-        # replace potentially harmful values from JSON
-        # before marking string as safe
-        .replace('>', '\\u003E')
-        .replace('<', '\\u003C')
-        .replace('&', '\\u0026')
-    )
+    # replace potentially harmful values from JSON
+    # before marking string as safe
+    return mark_safe(json.dumps(obj).translate(_json_script_escapes))
 
 
 def generate_js(default_urlresolver):
-    js_var_name = settings.JS_VAR_NAME
-    js_global_object_name = settings.JS_GLOBAL_OBJECT_NAME
-    script_prefix = settings.JS_SCRIPT_PREFIX
-    if script_prefix and not script_prefix.endswith('/'):
+    script_prefix = settings.JS_SCRIPT_PREFIX or get_script_prefix()
+    if not script_prefix.endswith('/'):
         script_prefix = f'{script_prefix}/'
-    elif not script_prefix:
-        script_prefix = get_script_prefix()
 
-    data = generate_json(default_urlresolver, script_prefix)
     js_content = loader.render_to_string(
         'django_reverse_js/url-resolver.tpl.js',
         {
-            'data': _safe_json(data),
-            'js_name': '.'.join([js_global_object_name, js_var_name]),
+            'data': _safe_json(generate_json(default_urlresolver, script_prefix)),
+            'js_name': f'{settings.JS_GLOBAL_OBJECT_NAME}.{settings.JS_VAR_NAME}',
             'reversejs_template': settings.JS_TEMPLATE,
         },
     )
@@ -139,7 +128,7 @@ def generate_cjs_module():
     return loader.render_to_string(
         'django_reverse_js/url-resolver.tpl.js',
         {
-            'data': "false",
+            'data': 'false',
             'js_name': 'module.exports',
             'reversejs_template': settings.JS_TEMPLATE,
         },
